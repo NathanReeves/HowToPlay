a0bab7a7ce1926e23fa2013f2697581421496c9d
NathanReeves <nathanreeves@Nathans-MacBook-Pro.local>
2017-02-23 13:29:47 -0700

    3rd person camera working, first person almost there

--- HowToPlayv.02/Assets/OurAssets/Scripts/SandFixedCamController.cs
+++ HowToPlayv.02/Assets/OurAssets/Scripts/SandFixedCamController.cs
@@ -11,26 +11,60 @@ public class SandFixedCamController : MonoBehaviour
 
     private Vector2 mouseLook;
     private Vector2 smoothV;
-    private GameObject character;
+	public GameObject character;
     
 	// Use this for initialization
 	void Start ()
     {
-        character = this.transform.gameObject;
+        //character = this.transform.gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+		/*
         // Look/aim with mouse
         var mouseChange = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
 
         mouseChange = Vector2.Scale(mouseChange, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
         smoothV.x = Mathf.Lerp(smoothV.x, mouseChange.x, 1f / smoothing);
         smoothV.y = Mathf.Lerp(smoothV.y, mouseChange.y, 1f / smoothing);
-        mouseLook += smoothV;
+        mouseLook += smoothV;*/
+		//transform.localRotation = Quaternion.AngleAxis(-mouseLook.x, Vector3.up);
 
-        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
-        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+		//3rd person
+		var rot = new Vector2(0f, 0f);
+		// rotates Camera Left
+		if (Input.GetAxis("Mouse X") < 0)
+		{
+			rot.x -= 1;
+		}
+		// rotates Camera Left
+		if (Input.GetAxis("Mouse X") > 0)
+		{
+			rot.x += 1;
+		}
+
+		// rotates Camera Up
+		if (Input.GetAxis("Mouse Y") < 0)
+		{
+			rot.y -= 1;
+		}
+		// rotates Camera Down
+		if (Input.GetAxis("Mouse Y") > 0)
+		{
+			rot.y += 1;
+		}
+
+
+		transform.Translate(rot);
+		transform.LookAt(character.transform);
+
+		///first person
+
+		//transform.Rotate(rot);
+		//transform.localRotation = Quaternion.Euler (rot);
+        //transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        //character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
     }
 }
