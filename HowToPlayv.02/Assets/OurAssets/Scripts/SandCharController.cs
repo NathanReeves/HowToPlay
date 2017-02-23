2908c0fee2a0cf789f3c62b7618bcf653ced64e2
Eric McNamara <Nishcov@gmail.com>
2017-02-22 22:32:05 -0700

    Sandbox Level
    Added sandbox level that demonstrates different camera changes and some
    basic movement changes. The first few "genres" work pretty well, but I'm
    having trouble with the aiming/turning for 3rd and 1st person.

---
+++ HowToPlayv.02/Assets/OurAssets/Scripts/SandCharController.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandCharController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpSpeed;
+    [SerializeField]
+    private Camera platCam;
+    [SerializeField]
+    private Camera twinCam;
+    [SerializeField]
+    private Camera thirdCam;
+    [SerializeField]
+    private Camera firstCam;
+
+    private Vector3 movement;
+    private Rigidbody playerRigidBody;
+    private int moveZone = 1;
+    private bool isGrounded;
+    Ray camRay;
+    private float camRayLength = 100f;
+    private int floorMask;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        playerRigidBody = GetComponent<Rigidbody>();
+
+        platCam = platCam.GetComponent<Camera>();
+        twinCam = twinCam.GetComponent<Camera>();
+        thirdCam = thirdCam.GetComponent<Camera>();
+        firstCam = firstCam.GetComponent<Camera>();
+
+        platCam.enabled = true;
+        twinCam.enabled = false;
+        thirdCam.enabled = false;
+        firstCam.enabled = false;
+    }
+
+    void FixedUpdate()
+    {
+        // Use platformer controls when in platformer zone
+        if (moveZone == 1 || moveZone == 2)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+
+            MovePlatformer(moveHorizontal);
+        }
+        // Use twin stick club controls when in twin stick club zone
+        else if (moveZone == 3)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVerical = Input.GetAxis("Vertical");
+
+            MoveTwinStickClub(moveHorizontal, moveVerical);
+        }
+        // Use twin stick shooting controls when in twin stick shoot zone
+        else if (moveZone == 4)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVerical = Input.GetAxis("Vertical");
+
+            MoveTwinStickShoot(moveHorizontal, moveVerical);
+            TurnTwinStickShoot();
+        }
+        // Use 3rd/1st person controls when in 3rd/1st person zone
+        else if (moveZone == 5 | moveZone == 6)
+        {
+            // Move player forward, back, left, right
+            float moveForwardBack = Input.GetAxis("Vertical");
+            float strafeLeftRight = Input.GetAxis("Horizontal");
+
+            MoveThirdFirst(moveForwardBack, strafeLeftRight);
+        }
+
+        // Check for jumping
+        if (isGrounded && moveZone > 1 && Input.GetAxis("Jump") > 0)
+        {
+            Jump();
+        }
+    }
+
+    private void MovePlatformer(float horiz)
+    {
+        // Set movement/rotation constraints
+        playerRigidBody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
+
+        // Set movement according to input
+        movement.Set(0f, 0f, horiz);
+
+        // Normalize movement
+        movement = movement * moveSpeed * Time.deltaTime;
+
+        // Move player
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+
+    private void MoveTwinStickClub(float horiz, float vert)
+    {
+        // Show player arm and sword
+        //player.renderer.child        
+
+        // Set rotation constraints
+        playerRigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        // Set movement according to input
+        movement.Set(-vert, 0f, horiz);
+
+        // Normalize movement
+        movement = movement.normalized * moveSpeed * Time.deltaTime;
+
+        // Move player
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+
+    private void MoveTwinStickShoot(float horiz, float vert)
+    {
+        // Show gun instead of sword
+        // blah
+
+        // Set rotation constraints
+        playerRigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        // Set movement according to input
+        movement.Set(-vert, 0f, horiz);
+
+        // Normalize movement
+        movement = movement.normalized * moveSpeed * Time.deltaTime;
+
+        // Move player
+        playerRigidBody.MovePosition(transform.position + movement);
+    }
+
+    private void TurnTwinStickShoot()
+    {
+        // Create a ray
+        camRay = Camera.current.ScreenPointToRay(Input.mousePosition);
+
+        // Setup RaycastHit to get info from ray
+        RaycastHit floorHit;
+
+        // If raycast hits anything...
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0; // Make sure player isn't leaning in y axis
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidBody.MoveRotation(newRotation);
+        }
+    }
+
+    private void MoveThirdFirst(float forBack, float strafe)
+    {
+        // Lock cursor to middle of screen
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Set rotation constraints
+        playerRigidBody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+        // Set movement according to input
+        //movement.Set(forBack, 0f, -strafe);
+
+        // Normalize movement
+        movement = movement.normalized * moveSpeed * Time.deltaTime;
+
+        // Move player
+        //playerRigidBody.MovePosition(transform.position + movement);
+        transform.Translate(forBack, 0f, -strafe);
+    }
+
+    // Triggers for camera and move changes
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enter2D"))
+        {
+            // Switch to 2D platformer controls (add jump)
+            moveZone = 2; 
+        }
+        if (other.gameObject.CompareTag("EnterTwinStickClub"))
+        {
+            // Switch to twin stick controls (only weapon swinging)
+            moveZone = 3;
+            // Switch to twin stick camera
+            platCam.enabled = false;
+            twinCam.enabled = true;
+            thirdCam.enabled = false;
+            firstCam.enabled = false;
+        }
+        if (other.gameObject.CompareTag("EnterTwinStickShoot"))
+        {
+            // Switch to twin stick shooter controls (add shooting)
+            moveZone = 4;
+        }
+        if (other.gameObject.CompareTag("Enter3rdPerson"))
+        {
+            // Switch to 3rd person / 1st person controls
+            moveZone = 5;
+            // Switch to 3rd person camera
+            platCam.enabled = false;
+            twinCam.enabled = false;
+            thirdCam.enabled = true;
+            firstCam.enabled = false;
+        }
+        if (other.gameObject.CompareTag("Enter1stPerson"))
+        {
+            // Switch to 1st person camera
+            platCam.enabled = false;
+            twinCam.enabled = false;
+            thirdCam.enabled = false;
+            firstCam.enabled = true;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    private void Jump()
+    {
+        isGrounded = false;
+        playerRigidBody.AddForce(Vector3.up * jumpSpeed);
+    }
+}
---
+++ HowToPlayv.02/Assets/OurAssets/Scripts/SandFixedCamController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandFixedCamController : MonoBehaviour
+{
+    [SerializeField]
+    private float sensitivity = 5.0f;
+    [SerializeField]
+    private float smoothing = 2.0f;
+
+    private Vector2 mouseLook;
+    private Vector2 smoothV;
+    private GameObject character;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        character = this.transform.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Look/aim with mouse
+        var mouseChange = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseChange = Vector2.Scale(mouseChange, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseChange.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseChange.y, 1f / smoothing);
+        mouseLook += smoothV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
