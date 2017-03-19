75d5095daeb8ad64e5110210c1f4f51b9ed41824
LarryNUofU <lepyzoom@live.com>
2017-03-18 22:04:48 -0600

    Added MainMenu scene and loadOnClick script in OurAssets folder

---
+++ HowToPlayv.02/Assets/OurAssets/loadOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadOnClick : MonoBehaviour {
+
+	public void LoadByIndex (int sceneIndex)
+	{
+		SceneManager.LoadScene (sceneIndex);
+	}
+
+	public void QuitGame(){
+		//source: http://answers.unity3d.com/questions/899037/applicationquit-not-working-1.html
+		// save any game data here
+		#if UNITY_EDITOR
+		// Application.Quit() does not work in the editor so
+		// UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+}
