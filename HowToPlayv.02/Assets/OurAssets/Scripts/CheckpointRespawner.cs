cef9b94921c5026c2ed26186090184e0f62367ff
Eric McNamara <Nishcov@gmail.com>
2017-04-13 13:16:49 -0600

    Fixed checkpoint system. Just need to place Checkpoint prefab, and everything else should work.

---
+++ HowToPlayv.02/Assets/OurAssets/Scripts/CheckpointRespawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointRespawner : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // If player collides with a checkpoint, the new spawnPoint is set to it
+            other.GetComponent<SandCharCheckpoint>().spawnPoint = transform;
+
+            // Also make checkpoint "disappear" and disable collider, so player can't reach this point more than once
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
