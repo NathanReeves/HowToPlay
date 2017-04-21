ffd344334c3702cc12b92ab414707d2c71190b1c
Eric McNamara <Nishcov@gmail.com>
2017-04-21 11:29:33 -0600

    Committing ignored files from yesterday

---
+++ HowToPlayv.02/Assets/OurAssets/Scripts/ConstrainBoxPosX.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstrainBoxPosX : MonoBehaviour
+{
+    private Rigidbody boxRigid;
+    private Vector3 boxPos;
+    [SerializeField]
+    private Vector3 maxBound;
+    [SerializeField]
+    private Vector3 minBound;
+
+    private void Awake()
+    {
+        boxRigid = GetComponent<Rigidbody>();
+        boxPos = boxRigid.position;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        boxPos.x = transform.position.x;
+
+        Debug.Log("Box pos z: " + boxPos);
+        Debug.Log("Max pos z: " + maxBound);
+        Debug.Log("Min pos z: " + minBound);
+
+        if (boxPos.x > maxBound.x)
+        {
+            boxRigid.MovePosition(maxBound);
+            boxRigid.AddForce(Vector3.zero);
+            boxRigid.velocity = Vector3.zero;
+        }
+        if (boxPos.x < minBound.x)
+        {
+            boxRigid.MovePosition(minBound);
+            boxRigid.AddForce(Vector3.zero);
+            boxRigid.velocity = Vector3.zero;
+        }
+	}
+}
---
+++ HowToPlayv.02/Assets/OurAssets/Scripts/ConstrainBoxPosZ.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstrainBoxPosZ : MonoBehaviour
+{
+    private Rigidbody boxRigid;
+    private Vector3 boxPos;
+    [SerializeField]
+    private Vector3 maxBound;
+    [SerializeField]
+    private Vector3 minBound;
+
+    private void Awake()
+    {
+        boxRigid = GetComponent<Rigidbody>();
+        boxPos = boxRigid.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        boxPos.z = transform.position.z;
+
+        Debug.Log("Box pos z: " + boxPos);
+        Debug.Log("Max pos z: " + maxBound);
+        Debug.Log("Min pos z: " + minBound);
+
+        if (boxPos.z > maxBound.z)
+        {
+            boxRigid.MovePosition(maxBound);
+            boxRigid.AddForce(Vector3.zero);
+            boxRigid.velocity = Vector3.zero;
+        }
+        if (boxPos.z < minBound.z)
+        {
+            boxRigid.MovePosition(minBound);
+            boxRigid.AddForce(Vector3.zero);
+            boxRigid.velocity = Vector3.zero;
+        }
+    }
+}
