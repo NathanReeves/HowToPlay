553c81c52112065e44af06e7ef2d0a0cd1451541
brad-kloos <kloosbw@gmail.com>
2017-04-25 15:50:20 -0600

    Current Everything

--- HowToPlayv.02/Assets/EndingTheGame.cs
+++ HowToPlayv.02/Assets/EndingTheGame.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
+//using UnityEditor;
 
 public class EndingTheGame : MonoBehaviour {
 	private UnityEngine.Video.VideoPlayer vPlayer;
